6487063284aeebe51cd407ad24cffeb10fdbc438
Maxoso41 <40214662+Maxoso41@users.noreply.github.com>
2024-12-12 09:35:19 +0100

    linter test

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import chalk from "chalk"
 
-console.log(chalk.cyan("It works"))
+console.log(chalk.cyan("It works"));
+
+let myUnusedVariable = 'hello';
+let myUsedVariable = 1;
+
+if (myUsedVariable == 1) {
+  console.log("it is one!")
+}
