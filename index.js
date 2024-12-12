07366540f4ec44ccb64f2fc77420f10a0996de13
Maxoso41 <40214662+Maxoso41@users.noreply.github.com>
2024-12-12 09:35:43 +0100

    linter fixed

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import chalk from "chalk"
 
 console.log(chalk.cyan("It works"));
 
-let myUnusedVariable = 'hello';
-let myUsedVariable = 1;
+const myUsedVariable = 1;
 
-if (myUsedVariable == 1) {
+if (myUsedVariable === 1) {
   console.log("it is one!")
 }
